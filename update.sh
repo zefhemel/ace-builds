4bb052d202e924ffef849ade0d8477972f74344b
Zef Hemel <zef@zef.me>
2014-07-10 14:44:49 +0200

    Moved all files to root to work as submodule

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-rm -rf ace
+rm *.js
 curl -L https://github.com/ajaxorg/ace/archive/master.tar.gz | tar xzf -
 cd ace-master
 npm install
 ./Makefile.dryice.js normal
-mv build/src ../ace
+mv build/src/* ../
 cd ..
-rm -rf ace/snippets
-rm ace/worker-* ace/theme-*
+rm -rf snippets
+rm worker-* theme-*
